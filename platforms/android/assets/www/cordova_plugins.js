228886a8663a8217d66b0e246f44808cc9de0019
mmosix <mmosix@gmail.com>
2016-07-31 23:33:43 +0000

    Plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,37 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-push-parse.PushNotification",
+        "file": "plugins/phonegap-plugin-push-parse/www/push.js",
+        "pluginId": "phonegap-plugin-push-parse",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-plugin-device": "1.1.2",
+    "phonegap-plugin-push-parse": "1.2.3"
 };
 // BOTTOM OF METADATA
 });
